bbd74b85576f614b807e86359fabde0d73e4abbb
syota-sidara <ss045.official@gmail.com>
2020-04-16 13:23:04 +0900

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about" no-prefetch>About</nuxt-link>
+      <nuxt-link :to="{name: 'users-list'}">UsersList</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{ subtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subtitle: "サブタイトルです"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
